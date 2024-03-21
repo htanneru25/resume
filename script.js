18e3d76ef8d6b4a7da9ea2da41598682ead03ad1
Harika Tanneru <155682289+htanneru25@users.noreply.github.com>
2024-03-20 20:37:38 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    var sidebar = document.querySelector('.sidebar');
+    var content = document.querySelector('.content');
+
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        content.style.marginLeft = '0';
+    } else {
+        sidebar.style.width = '250px';
+        content.style.marginLeft = '250px';
+    }
+}
